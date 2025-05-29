8e468c3be25605b920ab72969126d598c2603717
Rufaelu <niamraf12@gmail.com>
2025-05-29 20:20:25 +0300

    .

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Resource extends Model
 {
     protected $table = 'resources';
+    protected $fillable = [
+        'title',       // Add this
+        'description', // Include other fields that should be mass-assignable
+        'link',
+        'file_path',
+        'linkOrfile'   // Or whatever your field names are
+    ];
 }
